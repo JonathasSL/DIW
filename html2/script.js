888627bccdb1d6c6414b9af1f1a27ba5dc01aee4
Jônathas Leandro <jonathasbent@gmail.com>
2017-12-01 20:32:11 -0200

    execCreate

--- html2/script.js
+++ html2/script.js
@@ -1,5 +1,6 @@
 url = 'http://www.smartsoft.com.br/webservice/restifydb/empresa/puc_perfil?_view=json';
 
+//executa retrieve do CRUD 
 function execRetrieve() {
     var req = new XMLHttpRequest;
 
@@ -10,14 +11,14 @@ function execRetrieve() {
             var tabelaHTML = '';
 
             //montagem da tabela de perfis
-            tabelaHTML = '<table border="1">';
-            for (i = 0; i < dados.restify.rows.length; i++) {
-                tabelaHTML += '<tr><td><img src="' + dados.restify.rows[i].values.foto_url.value +'"></td>'
+            tabelaHTML = '<table border="2">';
+            for (i = 0; i < dados.restify.rowCount; i++) {
+                tabelaHTML += '<tr><td><img src="' + dados.restify.rows[i].values.foto_url.value + '"></td>'
                 tabelaHTML += '<td><h2>' + dados.restify.rows[i].values.nome.value + '</h2>';
                 tabelaHTML += '<p>Sexo: ' + dados.restify.rows[i].values.sexo.value + '<br>';
-                tabelaHTML += '<p>Data de nascimento: ' + dados.restify.rows[i].values.data_nascimento.value + '<br>';
-                tabelaHTML += '<p>Cidade: ' + dados.restify.rows[i].values.cidade.value + '<br>';
-                tabelaHTML += '<p>Website: ' + dados.restify.rows[i].values.site_url.value + '<br>';
+                tabelaHTML += 'Data de nascimento: ' + dados.restify.rows[i].values.data_nascimento.value + '<br>';
+                tabelaHTML += 'Cidade: ' + dados.restify.rows[i].values.cidade.value + '<br>';
+                tabelaHTML += 'Website: ' + dados.restify.rows[i].values.site_url.value + '<br></p>';
                 tabelaHTML += '</td></tr>';
             }
             tabelaHTML += '</table>';
@@ -29,4 +30,23 @@ function execRetrieve() {
 
     req.open('GET', url, true)
     req.send();
+}
+
+//executa o create do CRUD
+function execCreate(nome, sexo, data_nascimento, cidade, site_url, foto_url) {
+    var req = new XMLHttpRequest;
+    var dados = '_data={"nome":"' + nome.value + '", "sexo":"' + sexo.value + '", "data_nascimento":"' + data_nascimento.value + '", "cidade":"' + cidade.value + '", "site_url":"' + site_url.value + '", "foto_url":"' + foto_url.value + '"}';
+
+    req.onreadystatechange = function () {
+        //alert('ate aqui deu');
+        if ((req.readyState == 4) && (req.status == 200)) {
+            alert('Perfil incluido com sucesso.');
+
+            //recarrega dados do banco
+            execRetrieve();
+        }
+    }
+    req.open('POST', url, true);
+    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    req.send(dados);
 }
